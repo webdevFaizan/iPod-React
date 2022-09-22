f31ba76f40e108998ddfd868d6ef5f99028389b9
webdevFaizan <webdev.faizan@gmail.com>
2022-09-23 00:44:42 +0530

    I could get the details of the file in the firestore data base and then fill an array of certain length which contains the url as well as the name of the file, this is used to change the state of the list which will be passed on to the Play Music component so that we could play the music.

--- src/Screens/AllSongs.js
+++ src/Screens/AllSongs.js
@@ -3,35 +3,46 @@ import { getStorage,ref,getDownloadURL,listAll  } from "firebase/storage";
 import firebaseApp from '../Firebase';
 
 
-
-
 export default class AllSongs extends Component {
 
     constructor(){
         super();
-        this.temp_songs=[];
+        this.new_data_array=[];
         this.state = {
-            allSongs : [],
+            all_songs_list : [],
             loading : true
         }
     }
-    componentDidMount(){
+     componentDidMount(){
         // Initialize Cloud Storage and get a reference to the service
         const storage = getStorage(firebaseApp);
         const listRef = ref(storage, 'music');
         // console.log(gsReference);
         listAll(listRef)
         .then((res) => {
-          console.log(res) ;
+        //   console.log(res.items) ;
+            res.items.map(async (elem)=>{
+                // console.log(elem.name)
+                await getDownloadURL(elem)
+                .then((url) =>
+                {
+                    this.new_data_array.push({ name: elem.name, url: url });
+                    if (this.new_data_array.length === 2)//load the component when all the songs are added to the array.
+                    {
+                        this.setState({
+                            all_songs_list: this.new_data_array,
+                            loading: false
+                        },()=>{
+                            console.log(this.state.all_songs_list);
+                        });
+                    }
+                })
+                .catch((error) =>
+                {
+                    console.log(error);
+                })
+            })
         })
-
-        // getDownloadURL(ref(storage, '/Valkyries God of War - OST.mp3'))
-        // .then((url) => {
-        //    console.log(url);
-        //   })
-        //   .catch((error) => {
-        //     // Handle any errors
-        //   });
     }
   render() {
     return (
