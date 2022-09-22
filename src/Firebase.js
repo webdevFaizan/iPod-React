02613a34893629dbeb2e3f91bc954cdceb7ed394
webdevFaizan <webdev.faizan@gmail.com>
2022-09-22 23:44:12 +0530

    Working on the AllSongs.js file, I could successfully connect to the firestore cloud storage, remember it is storage not database, now we are trying to get the data to be downloaded and stream for our use. Not done yet, but trying to.

---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvtxuUCzc5vN6A-mNUl2khnGEib5K7h-w",
+  authDomain: "ipod-react-c473f.firebaseapp.com",
+  projectId: "ipod-react-c473f",
+  storageBucket: "ipod-react-c473f.appspot.com",
+  messagingSenderId: "244740859939",
+  appId: "1:244740859939:web:1a7f761ac08c1895956996"
+};
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/Screens/AllSongs.js
+++ src/Screens/AllSongs.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
+import { getStorage,ref,getDownloadURL  } from "firebase/storage";
+import firebaseApp from '../Firebase';
+
+
+
 
 export default class AllSongs extends Component {
+
+    constructor(){
+        super();
+        this.temp_songs=[];
+        this.state = {
+            allSongs : [],
+            loading : true
+        }
+    }
+    componentDidMount(){
+        // Initialize Cloud Storage and get a reference to the service
+        const storage = getStorage(firebaseApp);
+        const gsReference = ref(storage, 'gs://ipod-react-c473f.appspot.com/Valkyries_OST.mp3');
+        console.log(gsReference.name);
+        // getDownloadURL(ref(storage, '/Valkyries God of War - OST.mp3'))
+        // .then((url) => {
+        //    console.log(url);
+        //   })
+        //   .catch((error) => {
+        //     // Handle any errors
+        //   });
+    }
   render() {
     return (
       <div className='screen-game'>
